086253fb89f61ef05a4c35882b9d820c701f10e8
Th0rgal <thomas.marchand@tuta.io>
2020-08-12 16:34:52 +0200

    switch to functions

--- cashplace/src/routes/ticket/index.js
+++ cashplace/src/routes/ticket/index.js
@@ -1,31 +1,32 @@
-import { h, Component } from "preact";
+import { useState } from "preact/hooks";
 import style from "./style";
 
-export default class Ticket extends Component {
-  state = {};
+export default function Ticket({ requestsManager, id }) {
+  const [password, setPassword] = useState("");
 
-  componentDidMount() {
-    console.log(this.props.id);
-  }
+  const handleConnect = () => {
+    console.log("Entered password is", password);
+  };
 
-  // Note: `user` comes from the URL, courtesy of our router
-  render({ id }) {
-    return (
-      <div class={style.ticket}>
-        <h1>Blockchain address: {id}</h1>
-
-        <input type="password" placeholder="password"></input>
-        <div>
-          <button onClick={() => {}}>connect as spender</button>
-          You want to send BTC to someone and make sure to get what you paid
-          for.
-        </div>
-        <div>
-          <button onClick={() => {}}>connect as receiver</button>
-          You want to receive BTC from someone and make sure to receive your
-          money.
-        </div>
+  return (
+    <div class={style.ticket}>
+      <h1>Blockchain address: {id}</h1>
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+      <div>
+        <button onClick={() => handleConnect(true)}>connect as spender</button>
+        You want to send BTC to someone and make sure to get what you paid for.
       </div>
-    );
-  }
+      <div>
+        <button onClick={() => handleConnect(false)}>
+          connect as receiver
+        </button>
+        You want to receive BTC from someone and make sure to receive your
+        money.
+      </div>
+    </div>
+  );
 }
